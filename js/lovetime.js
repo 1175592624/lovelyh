d258f1035ba9ff48c2ff1e0a3456ae2bfb886ae5
Junior Yale <67873712+1175592624@users.noreply.github.com>
2020-09-19 19:44:29 +0800

    Add files via upload

--- js/lovetime.js
+++ js/lovetime.js
@@ -7,12 +7,11 @@ function lovetime() {
     var years = days * 365
     var today = new Date()
     var todayYear = today.getFullYear()
-    var todayMonth = today.getMonth()
+    var todayMonth = today.getMonth() + 1
     var todayDate = today.getDate()
     var todayHour = today.getHours()
     var todayMinute = today.getMinutes()
     var todaySecond = today.getSeconds()
-    // 修改这里时间即可 我的是2019.09.26
     var t1 = Date.UTC(2019, 09, 26, 00, 00, 00)
     var t2 = Date.UTC(todayYear, todayMonth, todayDate, todayHour, todayMinute, todaySecond)
     var diff = t2 - t1
